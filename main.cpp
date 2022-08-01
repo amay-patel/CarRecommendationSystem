03aeb0be7ad1b28f2a8e3fb9c995bb7788626a8a
Amay Patel <92037532+amay-patel@users.noreply.github.com>
2022-08-01 14:12:40 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -101,7 +101,7 @@ vector<int> takeRangeInput() {
     return responses;
 }
 
-unordered_map<double, string> cosine_similarility(vector<double> idealCar, unordered_map<string, vector<double>>& goodCars) {
+unordered_map<double, string> cosine_similarity(vector<double> idealCar, unordered_map<string, vector<double>>& goodCars) {
     unordered_map<double, string> cosCars;
     for(auto iter = goodCars.begin(); iter != goodCars.end(); ++iter) {
         double dotproduct = 0.0;
@@ -126,10 +126,59 @@ vector<double> mapToVec(unordered_map<double, string> hashmap) {
     return vec;
 }
 
+void merge(vector<double>& cars, int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+    vector<double> half(n1);
+    vector<double> otherHalf(n2);
+    for(int i = 0; i < n1; i++) {
+        half[i] = cars[left + i];
+    }
+    for(int j = 0; j < n2; j++) {
+        otherHalf[j] = cars[mid+1+j];
+    }
+    int i = 0;
+    int j = 0;
+    int k = left;
+    while(i < n1 && j < n2) {
+        if(half[i] <= otherHalf[j]) {
+            cars[k] = half[i];
+            i++;
+            k++;
+        }
+        else {
+            cars[k] = otherHalf[j];
+            j++;
+            k++;
+        }
+    }
+    while(i < n1) {
+        cars[k] = half[i];
+        i++;
+        k++;
+    }
+    while(j < n2) {
+        cars[k] = otherHalf[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(vector<double>& cars, int left, int right) {
+    if(left < right) {
+        int mid = left + (right - left)/2;
+        mergeSort(cars, left, mid);
+        mergeSort(cars, mid + 1, right);
+        merge(cars, left, mid, right);
+    }
+}
+
+
+
 int main() {
     vector<Car> cars;
     vector<double> idealCar;
-    vector<Car> goodCars;
+    unordered_map<string, vector<double>> goodCars;
     json list;
     ifstream fileOpener("cars.json");
     if(!fileOpener.is_open()) {
@@ -196,19 +245,56 @@ int main() {
         idealCar.push_back((rangeCityMPG[0]+rangeCityMPG[1])/2);
         idealCar.push_back((rangeHighwayMPG[0]+rangeHighwayMPG[1])/2);
         idealCar.push_back((priceRange[0]+priceRange[1])/2);
-        for(int i = 0; i < cars.size(); i++) {
-            if(find(brands.begin(), brands.end(), cars.at(i).brand) != brands.end()) {
+        if(brands.empty()) {
+            for(int i = 0; i < cars.size(); i++) {
                 if(find(years.begin(), years.end(), cars.at(i).year) != years.end()) {
                     if(cars.at(i).horsepower >= rangeHP[0] && cars.at(i).horsepower <= rangeHP[1]) {
                         if(cars.at(i).cityMPG >= rangeCityMPG[0] && cars.at(i).cityMPG <= rangeCityMPG[1]) {
                             if(cars.at(i).highwayMPG >= rangeHighwayMPG[0] && cars.at(i).highwayMPG <= rangeHighwayMPG[1]) {
                                 if(cars.at(i).price >= priceRange[0] && cars.at(i).price <= priceRange[1]) {
-                                    goodCars.push_back(cars.at(i));
+                                    goodCars[cars.at(i).model] = {cars.at(i).horsepower, cars.at(i).cityMPG,
+                                                                  cars.at(i).highwayMPG, cars.at(i).price};
                                 }
                             }
                         }
                     }
                 }
+            }
+        }
+        else {
+            for(int i = 0; i < cars.size(); i++) {
+                if(find(brands.begin(), brands.end(), cars.at(i).brand) != brands.end()) {
+                    if(find(years.begin(), years.end(), cars.at(i).year) != years.end()) {
+                        if(cars.at(i).horsepower >= rangeHP[0] && cars.at(i).horsepower <= rangeHP[1]) {
+                            if(cars.at(i).cityMPG >= rangeCityMPG[0] && cars.at(i).cityMPG <= rangeCityMPG[1]) {
+                                if(cars.at(i).highwayMPG >= rangeHighwayMPG[0] && cars.at(i).highwayMPG <= rangeHighwayMPG[1]) {
+                                    if(cars.at(i).price >= priceRange[0] && cars.at(i).price <= priceRange[1]) {
+                                        goodCars[cars.at(i).model] = {cars.at(i).horsepower, cars.at(i).cityMPG,
+                                                                      cars.at(i).highwayMPG, cars.at(i).price};
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    unordered_map<double, string> calc = cosine_similarity(idealCar, goodCars);
+    vector<double> sortVec = mapToVec(calc);
+    mergeSort(sortVec, 0, sortVec.size() - 1);
+    reverse(sortVec.begin(), sortVec.end());
+    if(sortVec.size() < 50) {
+        for(int i = 0; i < sortVec.size(); i++) {
+            if(calc.find(sortVec.at(i)) != calc.end()) {
+                cout << calc[sortVec.at(i)] << endl;
+            }
+        }
+    }
+    else {
+        for(int i = 0; i < 50; i++) {
+            if(calc.find(sortVec.at(i)) != calc.end()) {
+                cout << calc[sortVec.at(i)] << endl;
             }
         }
     }
