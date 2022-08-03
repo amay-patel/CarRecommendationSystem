4607a1822deb640b0a1f6cba3f09529e1f2e771e
Amay Patel <92037532+amay-patel@users.noreply.github.com>
2022-08-02 20:51:05 -0400

    Update Amaymain.cpp

--- Amaymain.cpp
+++ Amaymain.cpp
@@ -212,7 +212,6 @@ int main() {
      * goodCars hashmap is used to store cars that meet users requirements
      */
     vector<Car> cars;
-//    Tree tree(3,2);
     vector<double> idealCar;
     unordered_map<string, Tree*> hashmap;
     unordered_map<string, vector<double>> goodCars;
@@ -224,6 +223,8 @@ int main() {
     }
     fileOpener >> list;
     //this for loop will read in all of the statistics of the current car in the cars.json
+    Tree* temp = new Tree(3,2);
+    hashmap.insert({"Acura",temp});
     for(int i = 0; i < list.size(); i++) {
         string model = list[i].at("Identification.ID");
         string brand = list[i].at("Identification.Make");
@@ -233,17 +234,15 @@ int main() {
         int highwayMPG = list[i].at("Fuel Information.Highway mpg");
         string fuel = list[i].at("Fuel Information.Fuel Type");
         int price = list[i].at("Price");
-        cars.push_back(Car(model, brand, year, horsepower, cityMPG, highwayMPG, fuel, price));
-        //part of tree
+        cars.push_back(Car(model, brand, year, horsepower,
+                           cityMPG, highwayMPG, fuel, price));
         Car* car = new Car(model, brand, year, horsepower,
                            cityMPG, highwayMPG, fuel, price);
-        if(hashmap.find(brand) != hashmap.end()) {
-            hashmap[brand].Insert(car);
-        }
-        else {
-            Tree* temp = new Tree(3,2);
-            hashmap.insert({brand,temp});
-            temp.Insert(car);
+//        if(hashmap.find(brand) != hashmap.end()) {
+//            hashmap[brand]->Insert(car);
+//        }
+        if(brand == "Acura") {
+            temp->Insert(car);
         }
     }
     fileOpener.close();
@@ -294,35 +293,39 @@ int main() {
         //we do this by computing the mean of the values in each of the range vectors
         //then insert them into idealCar vector
 
-        if (priceRange.size() == 0) {
+        if(priceRange.size() == 0) {
             priceRange.push_back(4000);
             priceRange.push_back(840000);
-            idealCar.push_back((priceRange[0] + priceRange[1]) / 2);
-        } else {
-            idealCar.push_back((priceRange[0] + priceRange[1]) / 2);
+            idealCar.push_back((priceRange[0]+priceRange[1])/2);
         }
-        if (rangeCityMPG.size() == 0) {
+        else {
+            idealCar.push_back((priceRange[0]+priceRange[1])/2);
+        }
+        if(rangeCityMPG.size() == 0) {
             rangeCityMPG.push_back(22.4);
             rangeCityMPG.push_back(25.2);
-            idealCar.push_back((rangeCityMPG[0] + rangeCityMPG[1]) / 2);
-        } else {
-            idealCar.push_back((rangeCityMPG[0] + rangeCityMPG[1]) / 2);
+            idealCar.push_back((rangeCityMPG[0] + rangeCityMPG[1])/2);
         }
-        if (rangeHighwayMPG.size() == 0) {
+        else {
+            idealCar.push_back((rangeCityMPG[0] + rangeCityMPG[1])/2);
+        }
+        if(rangeHighwayMPG.size() == 0) {
             rangeHighwayMPG.push_back(23.9);
             rangeHighwayMPG.push_back(27.7);
-            idealCar.push_back((rangeHighwayMPG[0] + rangeHighwayMPG[1]) / 2);
-        } else {
-            idealCar.push_back((rangeHighwayMPG[0] + rangeHighwayMPG[1]) / 2);
+            idealCar.push_back((rangeHighwayMPG[0]+rangeHighwayMPG[1])/2);
         }
-        if (rangeHP.size() == 0) {
+        else {
+            idealCar.push_back((rangeHighwayMPG[0]+rangeHighwayMPG[1])/2);
+        }
+        if(rangeHP.size() == 0) {
             rangeHP.push_back(100);
             rangeHP.push_back(638);
-            idealCar.push_back((rangeHP[0] + rangeHP[1]) / 2);
-        } else {
-            idealCar.push_back((rangeHP[0] + rangeHP[1]) / 2);
+            idealCar.push_back((rangeHP[0] + rangeHP[1])/2);
         }
-    }
+        else {
+            idealCar.push_back((rangeHP[0] + rangeHP[1])/2);
+        }
+
         //bit confusing but it'll make sense
         //remember that user doesnt necessarily need to know what brand they're looking for
         //We are going through the entire cars vector and we need to check if the current car fits the criteria
@@ -332,27 +335,27 @@ int main() {
         //lastly the current car's price tag
         //The current car must fit all of the criteria listed by the user otherwise it will not be inserted into map
         //If all is satisfied, insert the car ID and then its horsepower, city mpg, highway mpg, and price into map
-//        if(brands.empty()) {
-//            // ****** Replace with Tree search method once implemented ******
-//            for(int i = 0; i < cars.size(); i++) {
-//                if(cars.at(i).getPrice() >= priceRange[0] && cars.at(i).getPrice() <= priceRange[1]) {
-//                    if(cars.at(i).getYear() >= years[0] && cars.at(i).getYear() <= years[1]) {
-//                        if(cars.at(i).getCityMpg() >= rangeCityMPG[0] && cars.at(i).getCityMpg() <= rangeCityMPG[1]) {
-//                            if(cars.at(i).getHighwayMpg() >= rangeHighwayMPG[0] && cars.at(i).getHighwayMpg() <= rangeHighwayMPG[1]) {
-//                                if (find(fuelTypes.begin(), fuelTypes.end(), cars.at(i).getFuel()) != fuelTypes.end()) {
-//                                    if (cars.at(i).getHorsepower() >= rangeHP[0] && cars.at(i).getHorsepower() <= rangeHP[1]) {
-//                                        goodCars[cars.at(i).getModel()] = {cars.at(i).getPrice(),
-//                                                                           cars.at(i).getCityMpg(),
-//                                                                           cars.at(i).getHighwayMpg(),
-//                                                                           cars.at(i).getHorsepower()};
-//                                    }
-//                                }
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//        }
+        if(brands.empty()) {
+            // ****** Replace with Tree search method once implemented ******
+            for(int i = 0; i < cars.size(); i++) {
+                if(cars.at(i).getPrice() >= priceRange[0] && cars.at(i).getPrice() <= priceRange[1]) {
+                    if(cars.at(i).getYear() >= years[0] && cars.at(i).getYear() <= years[1]) {
+                        if(cars.at(i).getCityMpg() >= rangeCityMPG[0] && cars.at(i).getCityMpg() <= rangeCityMPG[1]) {
+                            if(cars.at(i).getHighwayMpg() >= rangeHighwayMPG[0] && cars.at(i).getHighwayMpg() <= rangeHighwayMPG[1]) {
+                                if (find(fuelTypes.begin(), fuelTypes.end(), cars.at(i).getFuel()) != fuelTypes.end()) {
+                                    if (cars.at(i).getHorsepower() >= rangeHP[0] && cars.at(i).getHorsepower() <= rangeHP[1]) {
+                                        goodCars[cars.at(i).getModel()] = {cars.at(i).getPrice(),
+                                                                           cars.at(i).getCityMpg(),
+                                                                           cars.at(i).getHighwayMpg(),
+                                                                           cars.at(i).getHorsepower()};
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
 //        if(brands.empty()) {
 //            vector<Car*> decent = tree.Search(tree.getRoot(), years[0], years[1]);
 //            for (int i = 0; i < cars.size(); i++) {
@@ -375,106 +378,106 @@ int main() {
 //                }
 //            }
 //        }
-//        else {
-//            //This time if you the user specifies brands, we need to check if current car fits the list they provided in the question
-//            //We are going through the entire cars vector and we need to check if the current car fits the criteria
-//            //listed out by the user
-//            //First we check the year, then we check if the current car's horsepower is in the range, then the
-//            //current car's city mpg, then the current car's highway mpg, then the current car's fuel type,
-//            //lastly the current car's price tag
-//            //The current car must fit all of the criteria listed by the user otherwise it will not be inserted into map
-//            //If all is satisfied, insert the car ID and then its price, city mpg, highway mpg, and horsepower into map
-//            for(int i = 0; i < cars.size(); i++) {
-//                if (cars.at(i).getPrice() >= priceRange[0] && cars.at(i).getPrice() <= priceRange[1]) {
-//                    if (find(brands.begin(), brands.end(), cars.at(i).getBrand()) != brands.end()) {
-//                        if (cars.at(i).getYear() >= years[0] && cars.at(i).getYear() <= years[1]) {
-//                            if (cars.at(i).getCityMpg() >= rangeCityMPG[0] &&
-//                                cars.at(i).getCityMpg() <= rangeCityMPG[1]) {
-//                                if (cars.at(i).getHighwayMpg() >= rangeHighwayMPG[0] &&
-//                                    cars.at(i).getHighwayMpg() <= rangeHighwayMPG[1]) {
-//                                    if (find(fuelTypes.begin(), fuelTypes.end(), cars.at(i).getFuel()) !=
-//                                        fuelTypes.end()) {
-//                                        if (cars.at(i).getHorsepower() >= rangeHP[0] &&
-//                                            cars.at(i).getHorsepower() <= rangeHP[1]) {
-//                                            goodCars[cars.at(i).getModel()] = {cars.at(i).getPrice(),
-//                                                                               cars.at(i).getCityMpg(),
-//                                                                               cars.at(i).getHighwayMpg(),
-//                                                                               cars.at(i).getHorsepower()};
-//                                        }
-//                                    }
-//                                }
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//    }
-//    //then with the map that has all of the cars and stats that match user preferences,
-//    //we compute cosine similarity for each car
-//    unordered_map<double, string> calc = cosine_similarity(idealCar, goodCars);
-//    //below method will obtain all of the keys in the calc map
-//    //will be used in sorting methods
-//    //no changing...we need this to satisfy project requirements for 2 non-trivial sorting methods
-//    vector<double> sortVec = mapToVec(calc);
-//    //user gets to decide what sorting they would like to use
-//    //only selection is MergeSort or TimSort
-//    string input;
-//    cout << "What type of sort would you like to use: MergeSort or TimSort" << endl;
-//    while(true) {
-//        cin >> input;
-//        if(input == "MergeSort") {
-//            break;
-//        }
-//        else if(input == "TimSort") {
-//            break;
-//        }
-//        else {
-//            input = "";
-//        }
-//    }
-//    //we need to compute the time it takes for sorting to work
-//    //saw people in slack compute time for sorting methods...makes sense to do because
-//    //project requirements says we need to compare performance of two sorting algos or DSs
-//    //no changing
-//    auto start = high_resolution_clock::now();
-//    if(input == "MergeSort") {
-//        mergeSort(sortVec, 0, sortVec.size() - 1);
-//    }
-//    else if(input == "TimSort") {
-//        timSort(sortVec, sortVec.size());
-//    }
-//    //stop the clock. sorting is done
-//    auto stop = high_resolution_clock::now();
-//    //reverse the sortedVec to get the value with the highest cosine similarity
-//    //now we want to display top 50 cars that match user preferences
-//    //if there are not 50 cars, then just display the most that can be displayed
-//    //we use calc map to output the cars that best match user preferences (remember how calc map's values are car ID's)
-//    reverse(sortVec.begin(), sortVec.end());
-//    if(sortVec.size() < 50) {
-//        cout << endl;
-//        for(int i = 0; i < sortVec.size(); i++) {
-//            if(calc.find(sortVec.at(i)) != calc.end()) {
-//                cout << calc[sortVec.at(i)] << endl;
-//            }
-//        }
-//    }
-//    else {
-//        cout << endl;
-//        for(int i = 0; i < 50; i++) {
-//            if(calc.find(sortVec.at(i)) != calc.end()) {
-//                cout << calc[sortVec.at(i)] << endl;
-//            }
-//        }
-//    }
-//    cout << endl;
-//    //show how long it took for the sorting methods to do their thing
-//    //no changing we need this for project requirements
-//    if(input == "MergeSort") {
-//        cout << "MergeSort took " << duration_cast<microseconds>(stop - start).count() << " microseconds!" << endl;
-//    }
-//    else if(input == "TimSort") {
-//        cout << "TimSort took " << duration_cast<microseconds>(stop - start).count() << " microseconds!" << endl;
-//    }
+        else {
+            //This time if you the user specifies brands, we need to check if current car fits the list they provided in the question
+            //We are going through the entire cars vector and we need to check if the current car fits the criteria
+            //listed out by the user
+            //First we check the year, then we check if the current car's horsepower is in the range, then the
+            //current car's city mpg, then the current car's highway mpg, then the current car's fuel type,
+            //lastly the current car's price tag
+            //The current car must fit all of the criteria listed by the user otherwise it will not be inserted into map
+            //If all is satisfied, insert the car ID and then its price, city mpg, highway mpg, and horsepower into map
+            for(int i = 0; i < cars.size(); i++) {
+                if (cars.at(i).getPrice() >= priceRange[0] && cars.at(i).getPrice() <= priceRange[1]) {
+                    if (find(brands.begin(), brands.end(), cars.at(i).getBrand()) != brands.end()) {
+                        if (cars.at(i).getYear() >= years[0] && cars.at(i).getYear() <= years[1]) {
+                            if (cars.at(i).getCityMpg() >= rangeCityMPG[0] &&
+                                cars.at(i).getCityMpg() <= rangeCityMPG[1]) {
+                                if (cars.at(i).getHighwayMpg() >= rangeHighwayMPG[0] &&
+                                    cars.at(i).getHighwayMpg() <= rangeHighwayMPG[1]) {
+                                    if (find(fuelTypes.begin(), fuelTypes.end(), cars.at(i).getFuel()) !=
+                                        fuelTypes.end()) {
+                                        if (cars.at(i).getHorsepower() >= rangeHP[0] &&
+                                            cars.at(i).getHorsepower() <= rangeHP[1]) {
+                                            goodCars[cars.at(i).getModel()] = {cars.at(i).getPrice(),
+                                                                               cars.at(i).getCityMpg(),
+                                                                               cars.at(i).getHighwayMpg(),
+                                                                               cars.at(i).getHorsepower()};
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    //then with the map that has all of the cars and stats that match user preferences,
+    //we compute cosine similarity for each car
+    unordered_map<double, string> calc = cosine_similarity(idealCar, goodCars);
+    //below method will obtain all of the keys in the calc map
+    //will be used in sorting methods
+    //no changing...we need this to satisfy project requirements for 2 non-trivial sorting methods
+    vector<double> sortVec = mapToVec(calc);
+    //user gets to decide what sorting they would like to use
+    //only selection is MergeSort or TimSort
+    string input;
+    cout << "What type of sort would you like to use: MergeSort or TimSort" << endl;
+    while(true) {
+        cin >> input;
+        if(input == "MergeSort") {
+            break;
+        }
+        else if(input == "TimSort") {
+            break;
+        }
+        else {
+            input = "";
+        }
+    }
+    //we need to compute the time it takes for sorting to work
+    //saw people in slack compute time for sorting methods...makes sense to do because
+    //project requirements says we need to compare performance of two sorting algos or DSs
+    //no changing
+    auto start = high_resolution_clock::now();
+    if(input == "MergeSort") {
+        mergeSort(sortVec, 0, sortVec.size() - 1);
+    }
+    else if(input == "TimSort") {
+        timSort(sortVec, sortVec.size());
+    }
+    //stop the clock. sorting is done
+    auto stop = high_resolution_clock::now();
+    //reverse the sortedVec to get the value with the highest cosine similarity
+    //now we want to display top 50 cars that match user preferences
+    //if there are not 50 cars, then just display the most that can be displayed
+    //we use calc map to output the cars that best match user preferences (remember how calc map's values are car ID's)
+    reverse(sortVec.begin(), sortVec.end());
+    if(sortVec.size() < 50) {
+        cout << endl;
+        for(int i = 0; i < sortVec.size(); i++) {
+            if(calc.find(sortVec.at(i)) != calc.end()) {
+                cout << calc[sortVec.at(i)] << endl;
+            }
+        }
+    }
+    else {
+        cout << endl;
+        for(int i = 0; i < 50; i++) {
+            if(calc.find(sortVec.at(i)) != calc.end()) {
+                cout << calc[sortVec.at(i)] << endl;
+            }
+        }
+    }
+    cout << endl;
+    //show how long it took for the sorting methods to do their thing
+    //no changing we need this for project requirements
+    if(input == "MergeSort") {
+        cout << "MergeSort took " << duration_cast<microseconds>(stop - start).count() << " microseconds!" << endl;
+    }
+    else if(input == "TimSort") {
+        cout << "TimSort took " << duration_cast<microseconds>(stop - start).count() << " microseconds!" << endl;
+    }
     return 0;
 }
